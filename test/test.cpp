85cc56cb2cfaa92af3d61fcb02becc3e5159c71f
tryblt <iren09980.icloud.com>
2020-05-10 13:56:29 +0700

    Add another perimeter test

--- test/test.cpp
+++ test/test.cpp
@@ -2,6 +2,8 @@
 #include <gtest/gtest.h>
 
 TEST(PERIMETER, PerimeterCorrect) {
-    ASSERT_FLOAT_EQ(6.28, P(1.));
+    ASSERT_FLOAT_EQ(6.28, P(1));
     ASSERT_FLOAT_EQ(3.14, P(0.5));
+	ASSERT_FLOAT_EQ(0.05024, P(0.008));
+    ASSERT_FLOAT_EQ(39.4384, P(6.28));
 }
